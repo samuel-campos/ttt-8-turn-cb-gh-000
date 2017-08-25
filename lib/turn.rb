eee5a33fcd2e70b0bfc52719e8c2cc7b0080f51c
Sebastian Campos <21camposs@ptsdwebapps.com>
2017-08-25 03:40:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  elsif (board[index] == "X" || board[index] == "O")
+    return true
+  else
+    return nil
+  end
+end
+
+
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0,8)
     return true
